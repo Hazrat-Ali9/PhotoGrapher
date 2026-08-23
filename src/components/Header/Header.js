fe87b60c9c7c523fd50cb3b98536e2ecc52be750
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2026-08-23 09:42:51 +0600

    header :tada:

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ import logo from '../../images/logo.png'
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import { signOut } from 'firebase/auth';
-
+// header
 const Header = () => {
     const [user] = useAuthState(auth);
 
